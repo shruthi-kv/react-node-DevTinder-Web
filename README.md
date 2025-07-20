2af19ba837fbd50e207d11c39e54b96140e9c884
Shruthi KV <shruthikv@Mac.lan>
2025-07-20 19:49:14 +0530

    basic ui-navbar, body, footer

--- README.md
+++ README.md
@@ -5,4 +5,9 @@
 - install tailwind 
 - install daisyUI
 - add a navbar
+- install react-router-dom
+- create BrowserRouter, Routes, Route = /Body > RouteChildren
+- create Outlet;
+- create footer;
+
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import NavBar from './NavBar';
+import Body from './Body';
+import Login from './Login';
+import Profile from './Profile';
+import Footer from './Footer';
 function App() {
 
   return (
     <>
-      <div class="navbar bg-base-300 shadow-sm">
-        <div class="flex-1">
-          <a class="btn btn-ghost text-xl">DevTinder </a>
-        </div>
-        <div class="flex gap-2">
-          <div class="dropdown dropdown-end mx-5">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-              </div>
-            </div>
-            <ul
-              tabindex="0"
-              class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li>
-                <a class="justify-between">
-                  Profile
-                  <span class="badge">New</span>
-                </a>
-              </li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <BrowserRouter basename ='/'>
+      <Routes>
+           <Route path="/" element={<Body/>}>
+                <Route path="/login" element={<Login/>} />
+                <Route path="/profile" element={<Profile/>} />
+           </Route>
+          
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
